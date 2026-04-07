b73d11ae4b2bc7505bf964e46f2c98ec5459ef89
maarkkx <mark.gras.ferrer@gmail.com>
2026-04-07 22:11:22 +0200

    experience manager added

--- server/src/modules/games/guess_pokemon/repository.ts
+++ server/src/modules/games/guess_pokemon/repository.ts
@@ -1,4 +1,15 @@
 import prisma from '../../../../prisma/client';
+import { GameStatus } from '../../../../generated/prisma';
+
+export const getPokemonNameById = async (pokemonId: number) => {
+  return prisma.pokemon.findUnique({
+    where: { id: pokemonId },
+    select: {
+      id: true,
+      name: true
+    }
+  });
+};
 
 export const getRandomPokemon = async () => {
   const total = await prisma.pokemon.count(); //contar todos los pokemons de la bd
@@ -29,4 +40,54 @@ export const getRandomPokemon = async () => {
   });
 
   return pokemon[0];
+};
+
+export const createGame = async (userId: number, pokemonId: number, maxAttempts: number) => {
+  const game = await prisma.guessPokemonGame.create({
+    data: {
+      userId,
+      pokemonId,
+      maxAttempts,
+      remainingAttempts: maxAttempts,
+      status: GameStatus.ACTIVE
+    }
+  });
+
+  return game;
+};
+
+// Obtener partida por gameId
+export const getGameById = async (gameId: string) => {
+  return prisma.guessPokemonGame.findUnique({
+    where: { gameId }
+  });
+};
+
+// Actualizar la partida después de una respuesta
+export const updateGame = async (gameId: string,
+  data: {
+    remainingAttempts?: number;
+    lastGuess?: string;
+    status?: GameStatus;
+    xpEarned?: number;
+    lootboxesEarned?: number;
+  }
+) => {
+  return prisma.guessPokemonGame.update({
+    where: { gameId },
+    data
+  });
+};
+
+//mirar si tiene una partida activa
+export const getActiveGameByUserId = async (userId: number) => {
+  return prisma.guessPokemonGame.findFirst({
+    where: {
+      userId,
+      status: 'ACTIVE', // solo partidas activas
+    },
+    orderBy: {
+      startedAt: 'desc', // opcional: la más reciente si hay varias (aunque no debería)
+    },
+  });
 };
--- server/src/modules/games/guess_pokemon/service.ts
+++ server/src/modules/games/guess_pokemon/service.ts
@@ -1,5 +1,6 @@
-import { diff } from "node:util";
 import * as repository from "./repository";
+import * as types from "../constants/types"
+import * as expManager from "../experience_manager/manager";
 
 //Relacion dificultad vidas
 const livesDifficults : Record<string, number> = {
@@ -8,101 +9,163 @@ const livesDifficults : Record<string, number> = {
   'easy': 3
 }
 
-//Pokemon
-type Pokemon = {
-  id: number;
-  name: string;
-  urlImage: string | null;
-  hp: number;
-  atk: number;
-  def: number;
-  spAtk: number;
-  spDef: number;
-  speed: number;
-  types: any[];
-}
-
-let pokemon : Pokemon;
-
-//vidas
-let lives : number;
-
-async function randomPokemon() {
-  try {
-    pokemon = await repository.getRandomPokemon();
-  } catch {
-    console.log('Error getting pokemon')
-  }
-
-}
-
 //----------------------------------------------------
 //----------Funciones para empezar la partida----------
 //----------------------------------------------------
 
-export async function startGame(difficult: string): Promise<object> {
+export async function startGame(userId: number, difficult: string): Promise<object> {
   try {
-    await randomPokemon();
     //validacion de que la dificultad no esta vacia
     if (!difficult) {
       throw new Error("No difficulty selected");
     }
 
     //validacion de que la dificultad existe
-    let difficulties: String[] = ["easy", "medium", "hard"]; //dificultades disponibles
-    if (!difficulties.includes(difficult)) {
-      throw new Error("Difficulty does not exist");
+    if (!(difficult in livesDifficults)) {
+      console.log(livesDifficults);
+      console.log(difficult); 
+      throw new Error('Difficulty does not exist');
+    }
+
+    //crear nou pokemon
+    const pokemon: types.Pokemon = await repository.getRandomPokemon();
+    if (!pokemon) {
+      throw new Error('Error getting pokemon')
     }
 
     //llamar funcion de dificultad
-    placeLives(difficult);
+    const lives = livesDifficults[difficult];
 
-    let challenge = {
-        image: pokemon.urlImage, //url de la imagen del pokemon en la api
-        lives //intentos
+    //comprobar si el usuario tiene una partida activa
+    const activeGame = await repository.getActiveGameByUserId(userId);
+    if (activeGame) {
+      throw new Error('You already have an active game');
     }
-    return challenge;
+    //crear partida
+    const game = await repository.createGame(userId, pokemon.id, lives)
+
+    return {
+      gameId: game.gameId,
+      image: pokemon.urlImage,
+      lives: game.remainingAttempts
+    };
   } catch (error) {
     let errorMessage = {
-      message: error,
+      message: error instanceof Error ? error.message : error
     };
     console.log(error);
     return errorMessage;
   }
 }
 
-async function placeLives(difficult: string) {
-  lives = livesDifficults[difficult];
-}
-
 //----------------------------------------------------
 //-----------Funciones para las respuestas------------
 //----------------------------------------------------
-export function manageAnswer(answer : string) {
-  if (lives <= 0) {
-    return {
-      message: 'No tries remaining',
-      lives
-    }
+export async function manageAnswer(userId: number, gameId: string, answer: string) {
+
+//------------------------
+//-----Comprobaciones-----
+//------------------------
+  //check de los inputs:
+  if (!gameId) {
+    throw new Error('Game ID is null');
   }
-  if (checkAnswer(answer)) {
-    return {
-      message: 'Correct answer!',
-      lives
-    }
-  } else {
-    lives--;
-    return {
-      message: 'Incorrect answer',
-      lives
-    }
+  if (!userId) {
+    throw new Error('User Id is null')
   }
+  //veure la partida
+  const game = await repository.getGameById(gameId);
+
+  //comprobar si existe la partida
+  if (!game) {
+    throw new Error('Game not found');
+  }
+
+  //comporbar si el usuario de la partida
+  if (game.userId !== userId) {
+    throw new Error('Not your game');
+  }
+
+  //comprobar que la partida esta activa
+  if (game.status !== 'ACTIVE') {
+    return { message: 'Game already finished', status: game.status };
+  }
+
+//------------------------
+//-----Check Respuesta-----
+//------------------------
+
+  const pokemon = await repository.getPokemonNameById(game.pokemonId);
+
+  //comprobar que el pokemon
+  if (!pokemon) {
+    throw new Error('Pokemon not found');
+  }
+
+  //check respuesta
+  const isCorrect = answer.toLowerCase() === pokemon.name.toLowerCase();
+
+//------------------------
+//-----Datos partida------
+//------------------------
+
+  let remainingAttempts = game.remainingAttempts - (isCorrect ? 0 : 1); //restar vidas si es false
+  let status: types.GameStatus = 'ACTIVE';
+  let xpEarned = 0;
+
+//------------------------
+//--------Resultado--------
+//------------------------
+
+  if (isCorrect) {
+    status = 'WON';
+
+    //calcular experiencia segun dificultad
+    xpEarned = calculateXP(game.maxAttempts, game.remainingAttempts);
+
+    //añadir la experiencia al usuario
+    await expManager.addXP(userId, xpEarned);
+
+  } else if (remainingAttempts <= 0) {
+    status = 'LOST';
+  }
+
+//------------------------
+//-----Actualizar DB------
+//------------------------
+
+  //actualizar los datos de la partida
+  await repository.updateGame(gameId, {
+    remainingAttempts,
+    lastGuess: answer,
+    status,
+    xpEarned: xpEarned || undefined
+  });
+
+  return {
+    message: isCorrect ? 'Correct answer!' : 'Incorrect answer',
+    remainingAttempts,
+    status,
+    xpEarned
+  };
 }
 
-function checkAnswer(answer: string) : boolean {
-	if (answer.toLowerCase() == pokemon.name) {
-    return true
-  } else {
-    return false
+//----------------------------------------------------
+//-----------Funciones de experiencia------------------
+//----------------------------------------------------
+
+function calculateXP(maxAttempts : number, remainingAttempts : number): number {
+  switch (maxAttempts) {
+    case 3: 
+      return 50 + 10 * remainingAttempts; //easy
+
+    case 2: 
+      return 100 + 20 * remainingAttempts; //medium
+
+    case 1: 
+      return 250;  //hard
+
+    default: 
+      return 0;
   }
 }
